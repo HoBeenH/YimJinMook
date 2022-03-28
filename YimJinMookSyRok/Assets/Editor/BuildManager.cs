030581248c189f514dc5767edda1d64fc8fe4ae1
HoBeenH <87354491+HoBeenH@users.noreply.github.com>
2022-03-28 22:24:16 +0900

    [!] 커스텀 메뉴 생성

--- YimJinMookSyRok/Assets/Editor/BuildManager.cs
+++ YimJinMookSyRok/Assets/Editor/BuildManager.cs
@@ -1,18 +1,37 @@
-using System.Collections;
+#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
+using System.IO;
+using Sirenix.Utilities;
+using UnityEditor;
 using UnityEngine;
 
-public class BuildManager : MonoBehaviour
+namespace Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "BuildManager", menuName = "Build")]
+    public class BuildManager : ScriptableObject
     {
-        
-    }
+        private static BuildManager instance;
+        public BuildManager() => instance = this;
+        [SerializeField] private List<string> FileNames;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [MenuItem("Build/Window",false,1)]
+        public static void Build()
+        {
+            if (instance.FileNames.IsNullOrEmpty())
+            { Debug.LogAssertion(instance.FileNames.ToString()); return; }
+
+            var scenes = new string[instance.FileNames.Count];
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                scenes[i] = $"Assets\\Scenes\\{instance.FileNames[i]}.unity";
+            }
+            var flags = BuildOptions.Development;
+            var savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "\\TestBuild\\Test.exe");
+            BuildPipeline.BuildPlayer(scenes, savePath, BuildTarget.StandaloneWindows, flags);
+        }
     }
 }
+#endif
+
